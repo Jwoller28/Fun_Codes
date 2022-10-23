63432de294a41dd4e2cc469b181747d0216d2763
Jordan Woller <94810181+Jwoller28@users.noreply.github.com>
2022-10-23 17:35:21 -0400

    Update snake_game_human.py

--- snake_game_human.py
+++ snake_game_human.py
@@ -4,7 +4,8 @@ from enum import Enum
 from collections import namedtuple
 
 pygame.init()
-#font = pygame.font.Font('arial.ttf', 25)
+
+#arial.tff needed
 font = pygame.font.SysFont('arial', 25)
 
 class Direction(Enum):
